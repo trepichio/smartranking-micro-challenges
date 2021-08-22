071fb17c79610d130d2acc6bc12a40b89556349d
João Trepichio <trepichio@gmail.com>
2021-08-22 00:33:22 -0300

    add Event Responder get-challenges

--- src/challenges/challenges.controller.ts
+++ src/challenges/challenges.controller.ts
@@ -7,10 +7,35 @@ import {
   RmqContext,
 } from '@nestjs/microservices';
 import { ChallengesService } from './challenges.service';
+import { ChallengeInterface } from './interfaces/challenge.interface';
 
 @Controller('challenges')
 export class ChallengesController {
   constructor(private readonly challengesService: ChallengesService) {}
 
   private readonly logger = new Logger(ChallengesController.name);
+
+  @MessagePattern('get-challenges')
+  async getChallenge(
+    @Payload() payload: { challengeId: string; playerId: string },
+    @Ctx() context: RmqContext,
+  ): Promise<ChallengeInterface | ChallengeInterface[]> {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      const { playerId, challengeId } = payload;
+      if (challengeId) {
+        return await this.challengesService.getChallengeById(challengeId);
+      }
+
+      if (playerId) {
+        return await this.challengesService.getChallengesByPlayerId(playerId);
+      }
+
+      return await this.challengesService.getChallenges();
+    } finally {
+      await channel.ack(originalMessage);
+    }
+  }
 }
--- src/challenges/challenges.service.ts
+++ src/challenges/challenges.service.ts
@@ -5,12 +5,28 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ChallengeInterface } from './interfaces/challenge.interface';
 
 @Injectable()
 export class ChallengesService {
   constructor(
+    @InjectModel('Challenge')
+    private readonly challengeModel: Model<ChallengeInterface>,
+
   ) {}
 
   private readonly logger = new Logger(ChallengesService.name);
 
+  async getChallenges(): Promise<ChallengeInterface[]> {
+    try {
+      return await this.challengeModel.find().exec();
+    } catch (err) {
+      this.logger.error(
+        `Error getting challenges: ${JSON.stringify(err.message)}`,
+      );
+      throw new RpcException(err.message);
+    }
+  }
 }
