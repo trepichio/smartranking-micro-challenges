17d7365e4c2be2f941a532418d8d69d991919e01
João Trepichio <trepichio@gmail.com>
2021-08-22 00:16:28 -0300

    add databse configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from 'src/config/database';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({ expandVariables: true }),
+    MongooseModule.forRoot(process.env.DATABASE_URL, config),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
